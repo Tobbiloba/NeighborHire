fbcd529e9b8532c5d21a0ef86347293e38eafce2
Tobiloba <salauoluwatobiloba0@gmail.com>
2024-03-19 11:37:02 +0100

    feat: Built the menubar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
+import Layout from '@/layout/user/Layout'
+
 const Home = () => {
   return (
-    <>
-      <p>Home</p>
-    </>
+    <Layout>
+      <div className="min-h-[100vh] w-[100vw] heroBg pt-[6rem]">Home</div>
+    </Layout>
   )
 }
 export default Home
